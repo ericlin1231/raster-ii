eeebcb939a16ba7a0467587c244caf0945e0fd10
Alan Jian <alanjian85@outlook.com>
2024-12-26 10:56:23 +0800

    build: add Nix development environment

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Nix development environment of Raster II";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ circt sbt ];
+          shellHook = ''
+            export CHISEL_FIRTOOL_PATH=$(nix eval nixpkgs#circt.outPath --raw)/bin
+          '';
+        };
+      });
+}
