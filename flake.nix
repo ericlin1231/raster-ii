8407b876c2e3b44e87cde42d98b29b3f6db70a12
Alan Jian <alanjian85@outlook.com>
2025-01-20 18:18:41 +0800

    style: apply RFC-style formatting to flake.nix

--- flake.nix
+++ flake.nix
@@ -6,10 +6,19 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             circt
@@ -24,5 +33,6 @@
             export CHISEL_FIRTOOL_PATH=$(nix eval nixpkgs#circt.outPath --raw)/bin
           '';
         };
-      });
+      }
+    );
 }
