004a30187b20f7a9bbc8382ee1b43c2388da18e4
Alan Jian <alanjian85@outlook.com>
2025-01-29 21:13:23 +0800

    feat: add VGA controller and pattern generator

--- src/main/scala/Raster.scala
+++ src/main/scala/Raster.scala
@@ -1,19 +1,65 @@
 import chisel3._
-import chisel3.util.Counter
-import circt.stage.ChiselStage
+import chisel3.util._
+import _root_.circt.stage.ChiselStage
+
+class Vga extends Bundle {
+  val r = UInt(4.W)
+  val g = UInt(4.W)
+  val b = UInt(4.W)
+  val hsync = Bool()
+  val vsync = Bool()
+}
 
 class Raster extends Module {
   val io = IO(new Bundle {
-    val led = Output(Bool())
+    val vga = Output(new Vga)
+    val x = Output(UInt())
+    val y = Output(UInt())
+    val de = Output(Bool())
   })
 
-  val led = RegInit(false.B)
-  val (_, counterWrap) = Counter(true.B, 12_500_000)
-  when(counterWrap) {
-    led := ~led
-  }
+  val vgaTiming = new VgaTiming(
+    hactive = 640,
+    hfront = 16,
+    hsync = 96,
+    hback = 48,
+    vactive = 480,
+    vfront = 10,
+    vsync = 2,
+    vback = 33
+  )
+  val vgaController = Module(new VgaController(vgaTiming))
+  val x = vgaController.io.x
+  val y = vgaController.io.y
+  val de = vgaController.io.de
+  io.vga.hsync := vgaController.io.hsync
+  io.vga.vsync := vgaController.io.vsync
+  io.x := x
+  io.y := y
+  io.de := de
 
-  io.led := led
+  val rCntReg = RegInit(0.U(log2Up(40).W))
+  val rReg = RegInit(0.U(4.W))
+  val gCntReg = RegInit(0.U(log2Up(30).W))
+  val gReg = RegInit(0.U(4.W))
+  when(de) {
+    rCntReg := rCntReg + 1.U
+    when(rCntReg === 39.U) {
+      rCntReg := 0.U
+      rReg := rReg + 1.U
+    }
+
+    when(x === (vgaTiming.hactive - 1).U) {
+      gCntReg := gCntReg + 1.U
+      when(gCntReg === 29.U) {
+        gCntReg := 0.U
+        gReg := gReg + 1.U
+      }
+    }
+  }
+  io.vga.r := Mux(de, rReg, 0.U)
+  io.vga.g := Mux(de, gReg, 0.U)
+  io.vga.b := 0.U
 }
 
 object Raster extends App {
---
+++ src/main/scala/VgaController.scala
@@ -0,0 +1,48 @@
+import chisel3._
+import chisel3.util._
+
+case class VgaTiming(
+    hactive: Int,
+    hfront: Int,
+    hsync: Int,
+    hback: Int,
+    vactive: Int,
+    vfront: Int,
+    vsync: Int,
+    vback: Int
+)
+
+class VgaController(timing: VgaTiming) extends Module {
+  val hsyncStart = timing.hactive + timing.hfront
+  val hsyncEnd = hsyncStart + timing.hsync
+  val horizEnd = hsyncEnd + timing.hback
+
+  val vsyncStart = timing.vactive + timing.vfront
+  val vsyncEnd = vsyncStart + timing.vsync
+  val vertiEnd = vsyncEnd + timing.vback
+
+  val io = IO(new Bundle {
+    val x = Output(UInt(log2Up(horizEnd).W))
+    val y = Output(UInt(log2Up(vertiEnd).W))
+    val hsync = Output(Bool())
+    val vsync = Output(Bool())
+    val de = Output(Bool())
+  })
+
+  val xReg = RegInit(0.U(log2Up(horizEnd).W))
+  val yReg = RegInit(0.U(log2Up(vertiEnd).W))
+  xReg := xReg + 1.U
+  when(xReg === (horizEnd - 1).U) {
+    xReg := 0.U
+    yReg := yReg + 1.U
+    when(yReg === (vertiEnd - 1).U) {
+      yReg := 0.U
+    }
+  }
+  io.x := xReg
+  io.y := yReg
+
+  io.hsync := ~(hsyncStart.U <= xReg && xReg < hsyncEnd.U)
+  io.vsync := ~(vsyncStart.U <= yReg && yReg < vsyncStart.U)
+  io.de := xReg < timing.hactive.U && yReg < timing.vactive.U
+}
