0a31a5a533e41e28f4d15fd350d3ab9ec8bcbd66
Alan Jian <alanjian85@outlook.com>
2025-01-29 22:52:21 +0800

    feat: connect VGA signals to PMOD ports

--- synth/ulx3s/top.v
+++ synth/ulx3s/top.v
@@ -1,17 +1,18 @@
 module top(
   input clk_25mhz,
   input [6:0] btn,
-  output [7:0] led,
+  output [13:0] gp,
+  output [13:0] gn,
 );
-
-  reg [7:0] o_led;
 
   Raster raster(
     .clock(clk_25mhz),
     .reset(~btn[0]),
-    .io_led(o_led)
+    .io_vga_r(gn[10:7]),
+    .io_vga_g(gn[3:0]),
+    .io_vga_b(gp[10:7]),
+    .io_vga_hsync(gp[3]),
+    .io_vga_vsync(gp[2]),
   );
-
-  assign led = o_led;
 
 endmodule
