7b76b6335da9a213dfb15477375997c94434568a
Alan Jian <alanjian85@outlook.com>
2025-01-30 23:40:31 +0800

    feat: add preliminary PLL code for 125MHz clock

--- synth/ulx3s/top.v
+++ synth/ulx3s/top.v
@@ -1,9 +1,91 @@
+module clock_gen #(
+  parameter CLKI_DIV  = 1,
+  parameter CLKFB_DIV = 1,
+  parameter CLKOP_DIV = 1,
+  parameter CLKOP_CPHASE = 0
+) (
+  input  wire clk_in,
+  output wire clk_out,
+  output reg  clk_locked
+);
+
+  wire locked;
+
+  (* ICP_CURRENT="12" *)
+  (* LPF_RESISTOR="8" *)
+  (* MFG_ENABLE_FILTEROPAMP="1" *)
+  (* MFG_GMCREF_SEL="2" *)
+  EHXPLLL #(
+    .PLLRST_ENA("DISABLED"),
+    .INTFB_WAKE("DISABLED"),
+    .STDBY_ENABLE("DISABLED"),
+    .DPHASE_SOURCE("DISABLED"),
+    .OUTDIVIDER_MUXA("DIVA"),
+    .OUTDIVIDER_MUXB("DIVB"),
+    .OUTDIVIDER_MUXC("DIVC"),
+    .OUTDIVIDER_MUXD("DIVD"),
+    .CLKI_DIV(CLKI_DIV),
+    .CLKOP_ENABLE("ENABLED"),
+    .CLKOP_DIV(CLKOP_DIV),
+    .CLKOP_CPHASE(CLKOP_CPHASE),
+    .CLKOP_FPHASE(0),
+    .FEEDBK_PATH("CLKOP"),
+    .CLKFB_DIV(CLKFB_DIV)
+  ) pll_i (
+    .RST(1'b0),
+    .STDBY(1'b0),
+    .CLKI(clk_in),
+    .CLKOP(clk_out),
+    .CLKFB(clk_out),
+    .CLKINTFB(),
+    .PHASESEL0(1'b0),
+    .PHASESEL1(1'b0),
+    .PHASEDIR(1'b1),
+    .PHASESTEP(1'b1),
+    .PHASELOADREG(1'b1),
+    .PLLWAKESYNC(1'b0),
+    .ENCLKOP(1'b0),
+    .LOCK(locked)
+  );
+
+  reg locked_sync;
+  always @(posedge clk_out) begin
+    locked_sync <= locked;
+    clk_locked <= locked_sync;
+  end
+
+endmodule
+
 module top(
   input clk_25mhz,
   input [6:0] btn,
+  output [7:0] led,
   output [13:0] gp,
   output [13:0] gn,
 );
+
+  wire clk_125mhz;
+  clock_gen #(
+    .CLKI_DIV(1),
+    .CLKFB_DIV(5),
+    .CLKOP_DIV(5),
+    .CLKOP_CPHASE(0)
+  ) clock_gen_inst(
+    .clk_in(clk_25mhz),
+    .clk_out(clk_125mhz),
+    .clk_locked()
+  );
+
+  reg [31:0] cnt = 0;
+  reg led_status = 0;
+  always @(posedge clk_125mhz) begin
+    cnt <= cnt + 1;
+    if (cnt == 125000000) begin
+      led_status <= ~led_status;
+      cnt <= 0;
+    end
+  end
+  assign led = led_status;
 
   reg [3:0] r;
   reg [3:0] g;
