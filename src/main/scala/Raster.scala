13f2f68d57e20334cfa908c6790dc0fe572c944f
Alan Jian <alanjian85@outlook.com>
2025-02-01 21:29:14 +0800

    feat: add video pattern generator

--- src/main/scala/Raster.scala
+++ src/main/scala/Raster.scala
@@ -10,6 +10,30 @@ class Vga extends Bundle {
   val vsync = Bool()
 }
 
+class PatternGenerator(width: Int, height: Int) extends Module {
+  val io = IO(new Bundle {
+    val addr = Output(UInt(log2Up(width * height).W))
+    val data = Output(Vec(3, UInt(4.W)))
+  })
+
+  val xReg = RegInit(0.U(log2Up(width).W))
+  val yReg = RegInit(0.U(log2Up(height).W))
+  xReg := xReg + 1.U
+  when(xReg === width.U) {
+    xReg := 0.U
+    yReg := yReg + 1.U
+    when(yReg === height.U) {
+      yReg := 0.U
+    }
+  }
+  val pix = Wire(Vec(3, UInt(4.W)))
+  pix(0) := xReg / 40.U
+  pix(1) := yReg / 30.U
+  pix(2) := 0.U
+  io.addr := width.U * yReg + xReg
+  io.data := pix
+}
+
 class Raster extends Module {
   val io = IO(new Bundle {
     val vga = Output(new Vga)
@@ -38,13 +62,17 @@ class Raster extends Module {
   io.y := RegNext(y)
   io.de := de
 
-  val framebuffer =
-    SyncReadMem(vgaTiming.hactive * vgaTiming.vactive, Vec(3, UInt(4.W)))
+  val width = vgaTiming.hactive
+  val height = vgaTiming.vactive
+  val framebuffer = SyncReadMem(width * height, Vec(3, UInt(4.W)))
 
   val pix = framebuffer.read(vgaTiming.hactive.U * y + x)
   io.vga.r := Mux(de, pix(0), 0.U)
   io.vga.g := Mux(de, pix(1), 0.U)
   io.vga.b := Mux(de, pix(2), 0.U)
+
+  val patternGenerator = Module(new PatternGenerator(width, height))
+  framebuffer.write(patternGenerator.io.addr, patternGenerator.io.data)
 }
 
 object Raster extends App {
