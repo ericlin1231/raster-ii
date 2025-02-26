3efbf46f4c87f2fa52f67ea98a9e1e0210f29015
Alan Jian <alanjian85@outlook.com>
2025-02-26 12:29:11 +0800

    feat: implement BC1 compression algorithm

--- src/main/scala/display/Bc1Compressor.scala
+++ src/main/scala/display/Bc1Compressor.scala
@@ -14,45 +14,114 @@ class Bc1Compressor extends Module {
   })
 
   object State extends ChiselEnum {
-    val idle, read, write, done = Value
+    val idle, read0, minmax, read1, indices = Value
   }
   import State._
   val stateReg = RegInit(idle)
-  val blockReg = Reg(new Bc1Block)
+  val doneReg = RegInit(false.B)
   io.ready := false.B
-  io.done := false.B
-  io.addr := 0.U
-  io.block := blockReg
+  io.done := doneReg
+
+  val addrReg = Reg(UInt(4.W))
+  val minColorReg = Reg(MixedVec(UInt(5.W), UInt(6.W), UInt(5.W)))
+  val maxColorReg = Reg(MixedVec(UInt(5.W), UInt(6.W), UInt(5.W)))
+  val indicesReg = Reg(Vec(16, UInt(2.W)))
+  io.addr := addrReg
+  io.block.c0 := maxColorReg(0) ## maxColorReg(1) ## maxColorReg(2)
+  io.block.c1 := minColorReg(0) ## minColorReg(1) ## minColorReg(2)
+  io.block.indices := indicesReg
 
   switch(stateReg) {
     is(idle) {
       io.ready := true.B
       when(io.start) {
-        stateReg := read
+        stateReg := read0
+        doneReg := false.B
+        addrReg := 0.U
+        minColorReg := MixedVecInit(63.U, 127.U, 63.U)
+        maxColorReg := MixedVecInit(0.U, 0.U, 0.U)
       }
     }
-    is(read) {
-      io.addr := 0.U
-      stateReg := write
+    is(read0) {
+      io.addr := addrReg
+      stateReg := minmax
     }
-    is(write) {
-      val r = io.data(23, 16)
-      val g = io.data(15, 8)
-      val b = io.data(7, 0)
-      blockReg.c0 := r(7, 3) ## g(7, 2) ## b(7, 3)
-      blockReg.c1 := r(7, 3) ## g(7, 2) ## b(7, 3)
-      for (y <- 0 to 3) {
-        for (x <- 0 to 3) {
-          blockReg.indices((y << 2) + x) := 0.U
-        }
+    is(minmax) {
+      val colors = Wire(MixedVec(UInt(5.W), UInt(6.W), UInt(5.W)))
+      colors(0) := io.data(23, 19)
+      colors(1) := io.data(15, 10)
+      colors(2) := io.data(7, 3)
+      for (i <- 0 to 2) {
+        minColorReg(i) := Mux(
+          colors(i) < minColorReg(i),
+          colors(i),
+          minColorReg(i)
+        )
+        maxColorReg(i) := Mux(
+          colors(i) > maxColorReg(i),
+          colors(i),
+          maxColorReg(i)
+        )
       }
-      stateReg := done
+
+      addrReg := addrReg + 1.U
+      when(addrReg === 15.U) {
+        addrReg := 0.U
+        stateReg := indices
+      }.otherwise {
+        stateReg := read0
+      }
     }
-    is(done) {
-      io.ready := true.B
-      io.done := true.B
-      when(io.start) {
-        stateReg := read
+    is(read1) {
+      io.addr := addrReg
+      stateReg := indices
+    }
+    is(indices) {
+      val r = Wire(Vec(4, UInt(8.W)))
+      val g = Wire(Vec(4, UInt(8.W)))
+      val b = Wire(Vec(4, UInt(8.W)))
+
+      r(0) := maxColorReg(0) << 3 | maxColorReg(0)(4, 2)
+      g(0) := maxColorReg(1) << 2 | maxColorReg(1)(5, 4)
+      b(0) := maxColorReg(2) << 3 | maxColorReg(2)(4, 2)
+
+      r(1) := minColorReg(0) << 3 | minColorReg(0)(4, 2)
+      g(1) := minColorReg(1) << 2 | minColorReg(1)(5, 4)
+      b(1) := minColorReg(2) << 3 | minColorReg(2)(4, 2)
+
+      r(2) := ((r(0) << 2) +& r(0)) + ((r(1) << 1) +& r(1)) >> 3
+      g(2) := ((g(0) << 2) +& g(0)) + ((g(1) << 1) +& g(1)) >> 3
+      b(2) := ((b(0) << 2) +& b(0)) + ((b(1) << 1) +& b(1)) >> 3
+
+      r(3) := ((r(0) << 1) +& r(0)) + ((r(1) << 2) +& r(1)) >> 3
+      g(3) := ((g(0) << 1) +& g(0)) + ((g(1) << 2) +& g(1)) >> 3
+      b(3) := ((b(0) << 1) +& b(0)) + ((b(1) << 2) +& b(1)) >> 3
+
+      val d = Wire(Vec(4, SInt()))
+      for (i <- 0 to 3) {
+        d(i) :=
+          (r(i).zext - io.data(23, 16).zext).abs +
+            (g(i).zext - io.data(15, 8).zext).abs +
+            (b(i).zext - io.data(7, 0).zext).abs
+      }
+
+      val b0 = d(0) > d(3)
+      val b1 = d(1) > d(2)
+      val b2 = d(0) > d(2)
+      val b3 = d(1) > d(3)
+      val b4 = d(2) > d(3)
+
+      val x0 = b1 & b2
+      val x1 = b0 & b3
+      val x2 = b0 & b4
+      indicesReg(addrReg) := (x0 | x1) ## x2
+
+      addrReg := addrReg + 1.U
+      when(addrReg === 15.U) {
+        stateReg := idle
+        doneReg := true.B
+      }.otherwise {
+        stateReg := read1
       }
     }
   }
